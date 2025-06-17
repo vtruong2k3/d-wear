a9d203982116cefab5d9a4ee8b55ebe6b3ca37bc
vtruong2k3 <vtruong2k3@gmail.com>
2025-06-17 16:58:52 +0700

    Them folder contexts

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,5 @@
 
-import RoutesManager from './routes/routerManager'
+import RoutesManager from './routes/RouterManager'
 import { store } from './store/store'
 import { Provider } from 'react-redux';
 function App() {
