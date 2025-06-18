c4a541864db5fe0cded269773654a9dde7a06fe5
vtruong2k3 <vtruong2k3@gmail.com>
2025-06-19 00:04:04 +0700

    fix: loi router

--- Client/src/routes/ClientRouter.tsx
+++ Client/src/routes/ClientRouter.tsx
@@ -3,11 +3,13 @@ import ClientLayout from '../layouts/ClientLayout'
 import Home from '../pages/client/Home'
 
 
-const clientLayout = {
-    path: "/",
-    element: <ClientLayout />,
-    children: [
-        { path: "home", element: <Home /> }
-    ]
-}
-export default clientLayout
+const clientRouter = [
+    {
+        path: "/",
+        element: <ClientLayout />,
+        children: [
+            { path: "home", element: <Home /> }
+        ]
+    }
+]
+export default clientRouter
