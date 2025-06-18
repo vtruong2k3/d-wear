17c3f9d152d59bda376ce5c0e81bbf74b30ccca4
Duy Linh <Linhaz2005@gmail.com>
2025-06-19 00:18:44 +0700

    Lưu lại phần sửa ClientRouter.tsx

--- Client/src/routes/ClientRouter.tsx
+++ Client/src/routes/ClientRouter.tsx
@@ -1,13 +1,14 @@
-
-import ClientLayout from '../layouts/ClientLayout'
-import Home from '../pages/client/Home'
-
+import ClientLayout from "../layouts/ClientLayout";
+import Home from "../pages/client/Home";
+import Login from "../pages/client/Login";
 
 const clientLayout = {
-    path: "/",
-    element: <ClientLayout />,
-    children: [
-        { path: "home", element: <Home /> }
-    ]
-}
-export default clientLayout
+  path: "/",
+  element: <ClientLayout />,
+  children: [
+    { path: "home", element: <Home /> },
+    { path: "login", element: <Login /> },
+  ],
+};
+
+export default clientLayout;
