04ee158b6d9bfd663c06f72d7cbd35b75fa4884a
vtruong2k3 <vtruong2k3@gmail.com>
2025-06-19 00:31:00 +0700

    feat: add tailwind css setup

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
--- Client/src/pages/admin/auth/LoginAdmin.tsx
+++ Client/src/pages/admin/auth/LoginAdmin.tsx
@@ -1,3 +1,3 @@
 export default function loginAdmin() {
-    return <h1>Đây là trang login</h1>
+    return <h1 className="text-center text-red-800">Đây là trang login</h1>
 }
