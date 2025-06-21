e154c78a04614bcab4bf0cba7bf2f97fdb62a7b4
Thuan1611 <huythuan0977686@gmail.com>
2025-06-21 12:04:56 +0700

    feat: CRUD Products & Filter & Search

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,17 +1,4 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
-// import { BrowserRouter } from 'react-router-dom'
 
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>,
-// )
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
