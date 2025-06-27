de11ae6427a3f30767783a86b21a0a7c7762e488
vtruong2k3 <vtruong2k3@gmail.com>
2025-06-27 16:37:45 +0700

    feat: Them loading

--- Client/src/layouts/AdminLayout.tsx
+++ Client/src/layouts/AdminLayout.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import AdminHeader from "../components/Admin/Header/Headers";
 import AsideAdmin from "../components/Admin/SideBar/SideBar";
-
-export default function AdminLayout() {
+import { LoadingProvider } from '../contexts/LoadingContext';
+import GlobalLoading from '../components/Loading/GlobalLoading';
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
   const [collapsed, setCollapsed] = useState(false);
 
   const toggleCollapsed = () => {
@@ -11,26 +12,30 @@ export default function AdminLayout() {
   };
 
   return (
-    <div className="h-screen flex overflow-hidden">
-      {/* Sidebar cố định bên trái */}
-      <aside className={`shrink-0 transition-all duration-300 ${collapsed ? 'w-20' : 'w-64'}`}>
-        <AsideAdmin collapsed={collapsed} onCollapse={toggleCollapsed} />
-      </aside>
+    <LoadingProvider>
+      <GlobalLoading />
+      <div className="h-screen flex overflow-hidden">
+        {/* Sidebar cố định bên trái */}
+        <aside className={`shrink-0 transition-all duration-300 ${collapsed ? 'w-20' : 'w-64'}`}>
+          <AsideAdmin collapsed={collapsed} onCollapse={toggleCollapsed} />
+        </aside>
 
-      {/* Phần bên phải chứa header và main */}
-      <div className="flex flex-col flex-1 overflow-hidden">
-        {/* Header cố định trên cùng */}
-        <header className="shrink-0 shadow-sm ">
-          <AdminHeader collapsed={collapsed} onCollapse={toggleCollapsed} />
-        </header>
+        {/* Phần bên phải chứa header và main */}
+        <div className="flex flex-col flex-1 overflow-hidden">
+          {/* Header cố định trên cùng */}
+          <header className="shrink-0 shadow-sm ">
+            <AdminHeader collapsed={collapsed} onCollapse={toggleCollapsed} />
+          </header>
 
-        {/* Main content có thể cuộn */}
-        <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
-          <div className="bg-white rounded-xl shadow p-6 min-h-full">
-            <Outlet />
-          </div>
-        </main>
+          {/* Main content có thể cuộn */}
+          <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
+            <div className="bg-white rounded-xl shadow p-6 min-h-full">
+              <Outlet />
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
+
+    </LoadingProvider>
   );
 }
