48be7784d33cd357881abfd35df4dd6d91928338
hencua <nghiatdph44124@fpt.edu.vn>
2025-06-30 23:02:47 +0700

    ul all product

--- Client/src/routes/ClientRouter/routing.tsx
+++ Client/src/routes/ClientRouter/routing.tsx
@@ -3,7 +3,7 @@ import Home from "../../pages/client/Home/Home";
 import DetailProduct from "../../pages/client/ProductDetail/DetailProduct";
 import Login from "../../pages/client/Login/Login";
 import Register from "../../pages/client/Login/Register";
-
+import ListProduct from "../../pages/client/Product/ListProduct";
 import ShoppingCart from "../../pages/client/Cart/ShoppingCart";
 import Checkout from "../../pages/client/Order/Checkout";
 
@@ -16,12 +16,18 @@ const clientRoutes = [
         path: "",
         element: <Home />,
       },
+
+      {
+        path: "/product",
+        element: <ListProduct />,
+      },
+
       {
         path: "product/:id",
         element: <DetailProduct />,
 
       },
-      {
+      { 
         path: "shopping-cart",
         element: <ShoppingCart />,
       },
@@ -31,12 +37,12 @@ const clientRoutes = [
       },
       {
         path: "/login",
-        element: (<Login />),
+        element: <Login />,
       },
       {
         path: "/register",
-        element: (<Register />),
-      }
+        element: <Register />,
+      },
     ],
   },
 ];
